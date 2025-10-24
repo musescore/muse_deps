8ced1ee89e5c50d55ab7310e455b239f8a9a7d7e
Dmitry Makarenko <kryksyh@gmail.com>
2025-10-24 10:29:59 +0300

    Only link to wxbase

--- wxwidgets/3.1.3.9/wxwidgets.cmake
+++ wxwidgets/3.1.3.9/wxwidgets.cmake
@@ -22,14 +22,6 @@ function(wxwidgets_Populate remote_url local_path os arch build_type)
 
         set(wxwidgets_LIBRARIES
             ${local_path}/lib/libwx_baseu-3.1.so
-            ${local_path}/lib/libwx_baseu_net-3.1.so
-            ${local_path}/lib/libwx_baseu_xml-3.1.so
-            ${local_path}/lib/libwx_gtk2u_adv-3.1.so
-            ${local_path}/lib/libwx_gtk2u_aui-3.1.so
-            ${local_path}/lib/libwx_gtk2u_core-3.1.so
-            ${local_path}/lib/libwx_gtk2u_html-3.1.so
-            ${local_path}/lib/libwx_gtk2u_qa-3.1.so
-            ${local_path}/lib/libwx_gtk2u_xrc-3.1.so
         )
         set(wxwidgets_INSTALL_LIBRARIES ${wxwidgets_LIBRARIES})
 
@@ -60,14 +52,6 @@ function(wxwidgets_Populate remote_url local_path os arch build_type)
 
         set(wxwidgets_LIBRARIES
             ${local_path}/lib/libwx_baseu-3.1.dylib
-            ${local_path}/lib/libwx_baseu_net-3.1.dylib
-            ${local_path}/lib/libwx_baseu_xml-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_adv-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_aui-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_core-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_html-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_qa-3.1.dylib
-            ${local_path}/lib/libwx_osx_cocoau_xrc-3.1.dylib
         )
         set(wxwidgets_INSTALL_LIBRARIES ${wxwidgets_LIBRARIES})
 
@@ -98,26 +82,10 @@ function(wxwidgets_Populate remote_url local_path os arch build_type)
 
         set(wxwidgets_LIBRARIES
             ${local_path}/lib/vc_x64_dll/wxbase31u${suffix}.lib
-            ${local_path}/lib/vc_x64_dll/wxbase31u${suffix}_net.lib
-            ${local_path}/lib/vc_x64_dll/wxbase31u${suffix}_xml.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_adv.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_aui.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_core.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_html.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_qa.lib
-            ${local_path}/lib/vc_x64_dll/wxmsw31u${suffix}_xrc.lib
         )
 
         set(wxwidgets_INSTALL_LIBRARIES
-            ${local_path}/lib/vc_x64_dll/wxbase313u${suffix}_net_vc_x64_custom.dll
             ${local_path}/lib/vc_x64_dll/wxbase313u${suffix}_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxbase313u${suffix}_xml_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_adv_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_aui_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_core_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_html_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_qa_vc_x64_custom.dll
-            ${local_path}/lib/vc_x64_dll/wxmsw313u${suffix}_xrc_vc_x64_custom.dll
         )
 
     else()
