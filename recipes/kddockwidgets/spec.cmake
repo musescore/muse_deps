5380b98975dbe785d87cc473554eb80575262d4d
Eism <eismailzada@gmail.com>
2026-06-26 12:14:00 +0300

    fixed sha256

--- recipes/kddockwidgets/spec.cmake
+++ recipes/kddockwidgets/spec.cmake
@@ -1,5 +1,5 @@
 set(DEP_VERSION 2.4)
 
 set(DEP_SOURCES
-    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/5937b539edb12940d70e746c35f7887592b6afe6.tar.gz|df53c4d4c856b3741b5cc30e0942843568b8b1a662f8ed1d04c694564b0e1386"
+    "kddockwidgets|tarball|https://github.com/musescore/KDDockWidgets/archive/5937b539edb12940d70e746c35f7887592b6afe6.tar.gz|ff924045369e0b4ae134f34bbb2c777e2d91220cb35eaa451add03cf6453cfb5"
 )
