0f56838a2e0c867fb045ca2e458df68c823db1d5
Dmitry Makarenko <kryksyh@gmail.com>
2026-09-06 13:38:46 +0300

    twolame: allow using system copy

--- recipes/twolame/meta.cmake
+++ recipes/twolame/meta.cmake
@@ -1,1 +1,4 @@
-set(DEP_KIND source)
+set(DEP_TARGET twolame)
+set(DEP_LIBS twolame)
+set(DEP_SYSTEM_HEADER twolame.h)
+set(DEP_STATIC_WINDOWS ON)
--- recipes/twolame/spec.cmake
+++ recipes/twolame/spec.cmake
@@ -1,5 +1,6 @@
 set(DEP_VERSION 0.3.13)
 
-set(DEP_SOURCES
-    "twolame|tarball|https://downloads.sourceforge.net/project/twolame/twolame/0.3.13/twolame-0.3.13.tar.gz|98f332f48951f47f23f70fd0379463aff7d7fb26f07e1e24e42ddef22cc6112a"
-)
+set(DEP_SOURCE_URL    "https://downloads.sourceforge.net/project/twolame/twolame/0.3.13/twolame-0.3.13.tar.gz")
+set(DEP_SOURCE_SHA256 "98f332f48951f47f23f70fd0379463aff7d7fb26f07e1e24e42ddef22cc6112a")
+
+set(DEP_LICENSE_FILES COPYING)
