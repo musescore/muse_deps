ee98152c3212b99f0aad13bfcdd6ea0c533f8e38
Dmitry Makarenko <kryksyh@gmail.com>
2026-09-24 09:45:38 +0300

    Update sqlite to 3.53.4

--- recipes/sqlite/spec.cmake
+++ recipes/sqlite/spec.cmake
@@ -1,6 +1,4 @@
-set(DEP_VERSION 3.50.4)
+set(DEP_VERSION 3.53.4)
 
-set(DEP_SOURCE_URL    "https://sqlite.org/2025/sqlite-amalgamation-3500400.zip")
-set(DEP_SOURCE_SHA256 "1d3049dd0f830a025a53105fc79fd2ab9431aea99e137809d064d8ee8356b032")
-
-set(DEP_PATCHES patch/0001-unused-parameter-warnings.patch)
+set(DEP_SOURCE_URL    "https://sqlite.org/2026/sqlite-amalgamation-3530400.zip")
+set(DEP_SOURCE_SHA256 "1e71ddf93849c6a6ecf58b827c0692073d2dd7ee40196158068f7b29f422e87d")
